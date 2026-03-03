c192158139167809e37dc1f1a6d7aee0d38d3495
root <root@ip-172-31-4-178.ap-south-1.compute.internal>
2026-03-03 14:17:12 +0000

    Fix: Switching to t3.micro to fit within 16 vCPU limit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # EC2 ఇన్‌స్టాన్స్ క్రియేషన్
 resource "aws_instance" "bastion_host" {
   ami                         = "ami-051a31ab2f4d498f5" # మీరు పంపిన చిత్రంలోని Red Hat Enterprise Linux 10 AMI
-  instance_type               = "c7i-flex.large"              # మీరు కోరిన ఇన్‌స్టాన్స్ టైప్
+  instance_type               = "t3.micro"              # మీరు కోరిన ఇన్‌స్టాన్స్ టైప్
   key_name                    = "ARGO"                  # మీ కీపెయిర్ పేరు
   subnet_id                   = aws_subnet.public_subnets[0].id # మనం ముందు క్రియేట్ చేసిన Pub-Sub-1
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id] # మనం క్రియేట్ చేసిన Bastion-SG
